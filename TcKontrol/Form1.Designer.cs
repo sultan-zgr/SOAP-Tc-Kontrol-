2ba8131db61f06ca434071b9a54aa049fcaa6f02
sultan-zgr <88083356+sultan-zgr@users.noreply.github.com>
2023-03-11 15:30:48 +0300

    Add project files.

---
+++ TcKontrol/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TcKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTc = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtYıl = new System.Windows.Forms.TextBox();
+            this.bKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SOYAD:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "YIL:";
+            // 
+            // txtTc
+            // 
+            this.txtTc.Location = new System.Drawing.Point(73, 12);
+            this.txtTc.Name = "txtTc";
+            this.txtTc.Size = new System.Drawing.Size(194, 20);
+            this.txtTc.TabIndex = 4;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(73, 38);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(194, 20);
+            this.txtAd.TabIndex = 5;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(73, 64);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(194, 20);
+            this.txtSoyad.TabIndex = 6;
+            // 
+            // txtYıl
+            // 
+            this.txtYıl.Location = new System.Drawing.Point(73, 90);
+            this.txtYıl.Name = "txtYıl";
+            this.txtYıl.Size = new System.Drawing.Size(194, 20);
+            this.txtYıl.TabIndex = 7;
+            // 
+            // bKaydet
+            // 
+            this.bKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bKaydet.Location = new System.Drawing.Point(192, 130);
+            this.bKaydet.Name = "bKaydet";
+            this.bKaydet.Size = new System.Drawing.Size(75, 23);
+            this.bKaydet.TabIndex = 8;
+            this.bKaydet.Text = "Kaydet";
+            this.bKaydet.UseVisualStyleBackColor = true;
+            this.bKaydet.Click += new System.EventHandler(this.bKaydet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 168);
+            this.Controls.Add(this.bKaydet);
+            this.Controls.Add(this.txtYıl);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtTc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTc;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtYıl;
+        private System.Windows.Forms.Button bKaydet;
+    }
+}
+
---
+++ TcKontrol/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bKaydet_Click(object sender, EventArgs e)
+        {
+            long kimlikNo = Convert.ToInt64(txtTc.Text);
+            int yil = Convert.ToInt32(txtYıl.Text);
+            bool durum;
+
+            try
+            {
+                using (TcDogrula.KPSPublicSoapClient service = new TcDogrula.KPSPublicSoapClient { })
+                {
+                    durum = service.TCKimlikNoDogrula(kimlikNo, txtAd.Text, txtSoyad.Text, yil);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (durum == true)
+            {
+                MessageBox.Show("Kayıt bulundu.");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt bulunamadı");
+            }
+        }
+    }
+}
